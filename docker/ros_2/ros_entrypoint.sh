08c33b940c017b2c97a71d970ef43734ecb3befe
joao41 <joao41-second@users.noreply.github.com>
2025-12-31 17:52:39 +0000

    add robot

--- docker/ros_2/ros_entrypoint.sh
+++ docker/ros_2/ros_entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo ¨ola user¨
+echo ¨start the source¨
 set -e
 
 source /opt/ros/foxy/setup.bash
-exec "$@"
+
+#exec "$@"
+exec gazebo --verbose  -s libgazebo_ros_factory.so /app/simulator/words/word1.world 
